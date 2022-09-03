09b8c1f1ab6341156a3d4ce5016765abdc7f7386
Ronald Cole <78932246+RonnieCole1@users.noreply.github.com>
2022-09-03 15:01:12 -0400

    Added h, Phase1, Phase 2, Phase3, and types files.
    Added pcb.c and other .c and .h files. pcb.c is incomplete and lacks documentation.

---
+++ Phase1/pcb.c
@@ -0,0 +1,69 @@
+int emptyProcQ(pcb_t *tp) {
+    return(tp == NULL);
+}
+
+pcb_t *headProc(pcb_t *tp) {
+    if(emptyProcQ(tp)) {
+        return NULL;
+    }
+    return (tp -> p_next);
+}
+
+pcb_t *mkEmptyProcQ() {
+    return NULL;
+}
+
+void insertProcQ(pcb_t **tp, pcb_t *p) {
+    if (emptyProc(tp) {
+        *(tp) = p;
+        p -> p_next = p;
+        p -> p_prev = p;
+        return;
+    }
+    *(tp) = R;
+    R -> p_next = Q;
+    R -> p_prev = N;
+    R -> p_next = N;
+    N -> p_prev = Q;
+    N -> p_next = Q;
+    R -> p_next = NULL;
+    R -> p_prev = NULL;
+
+}
+
+initPcbs() {
+    static pcb_t initArray[MAXPCBCNT];
+    for (int i = 0; i < MAXPCBCNT; i++) {
+        allocPcbs(&initArray[i]);
+    }
+}
+
+pcb_t *removeProcQ(pcb_t **tp) {
+    if (emptyProcQ(tp)) {
+        return NULL;
+    } else {
+        *(tp) = P;
+        pcb_t temp = p -> p_prev;
+        temp = temp -> p_next;
+        p = NULL;
+        p -> p_prev = Q;
+        p -> p_next = R;
+        Q -> p_next = R;
+        R -> p_prev = Q;
+        if (tp -> p_next == p) {
+            tp -> p_next = Q;
+        }
+        return (temp);
+    }
+}
+
+/*         Trees                 */
+
+int emptyChild(pcb_t *p) {
+    return(p -> p_child = NULL);
+}
+
+void insertChild (pcb_t *prnt, pcb_t *p){
+    p -> p_prnt = prnt;
+    prnt -> p_child = p;
+}
