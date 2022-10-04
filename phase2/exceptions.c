dd274a4b1a00e78588f11093e08f63b158307f6a
Ronald Cole <78932246+RonnieCole1@users.noreply.github.com>
2022-10-04 15:46:10 -0400

    Made "Progress" on all sorts of things

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -1,9 +1,68 @@
 #include "../h/pcb.h"
+#include "../h/asl.h"
 #include "../h/types.h"
 #include "../h/const.h"
 #include "initial.c"
 
-/* Sys1 */
+void exceptionHandler()
+{
+    if(/*Examine State Reg to check if in Kernal*/)
+    {
+        
+    } else {
+        /* I want to do this as a switch case... but how?*/
+        /*Interupts*/
+        if(Cause.ExcCode == 0)
+        {
+
+        }
+        /*TLB Exceptions*/
+        if(Cause.ExcCode <= 3 && Cause.ExcCode >= 1)
+        {
+            
+        }
+        /*Program Traps*/
+        if((Cause.ExcCode <= 4 && Cause.ExcCode >= 7 )
+        && (Cause.ExcCode <= 9 && Cause.ExcCode >= 12 ))
+        {
+            
+        }
+        /*SYSCALL*/
+        if(Cause.ExcCode == 8)
+        {
+            /* How to define SYSCall?*/
+            /*SYSCALL(x,x,x,x)*/
+        }
+    }
+}
+/*These fields don't match the book, but it
+comes as close as I could...*/
+void SYSCALL(int SYSNUM, int sema4, blah, blah) 
+{
+    switch(SYSNUM) 
+    {
+        case CREATEPROCESS:
+            Create_ProcessP();
+        case TERMINATEPROCESS:
+            Terminate_Process();
+        case PASSEREN:
+            wait(sema4);
+        case VERHOGEN:
+            signal(sema4);
+        case WAITIO:
+            Wait_for_IO_Device();
+        case GETCPUTIME:
+            Get_CPU_Time(p);
+        case WAITCLOCK:
+            Wait_For_Clock();
+        case GETSUPPORTPRT:
+            void Get_SUPPORT_Data();
+    }
+
+    LDST();
+}
+
+/*SYS1*/
 void Create_ProcessP()
 {
     /*Initialize fields of p*/
@@ -37,6 +96,7 @@ pcb_t* wait(sema4)
         pcb_t *p = removeProcQ(&(sema4));
         insertBlocked(&(sema4),p);
     }
+    BlockedSYS(p);
     return currentProc;
 }
 
@@ -55,7 +115,9 @@ pcb_t* signal(sema4)
 /* Sys5 */
 void Wait_for_IO_Device()
 {
-
+    BlockedSYS(currentProc);
+    SYSCALL(PASSEREN, iosema4, 0, 0);
+    /*Need to handle subdevices*/
 }
 
 /* Sys6 */
@@ -87,11 +149,26 @@ int Get_CPU_Time(pcb_t p)
 void Wait_For_Clock()
 {
     /* Define pseudoClockSema4 */
-    wait(pseudoClockSema4);
+    SYSCALL(PASSEREN, pseudoClockSema4, 0, 0);
+    /*Do this every 100ms*/
+    SYSCALL(VERHOGEN, pseudoClockSema4, 0, 0);
+    BlockedSYS(currentProc);
 }
 
 /* Sys8 */
 void Get_SUPPORT_Data()
 {
     return currentProc->p_supportStruct;
+}
+
+/*Used for syscalls that block*/
+void BlockedSYS(pcb_t p)
+{
+    /*Refer to 3.5.11 to complete this code */
+    s_pc = s_pc + 4;
+    p->p_s = /*BOISDATAPAGESTATE*/
+    p->p_time = p->p_time + intervaltimer;
+    insertBlocked(currentProc);
+    scheduler()
+    /****************************************/
 }
--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -1,10 +1,30 @@
+#include "../h/pcb.h"
 #include "../h/types.h"
 #include "../h/const.h"
-#include "../h/pcb.h"
 #include "initial.c"
 
-void scheduler(){
-    if(currentProc == 0){
-        
+
+void scheduler() {
+    if(emptyProcQ(readyQue))
+    {
+        if(procssCnt == 0)
+        [
+            /*Invoke HAULT*/
+        ]
+        if(procssCnt > 0 && softBlockCnt > 0)
+        {
+            /*set Status reg to enable interupts and
+            disable the PLT or load it with a very large
+            value */
+            /*Invoke WAIT*/
+        }
     }
+    pcb_t *p = removeProcQ(readyQue);
+    insertChild(currentProc, p);
+
+    /*Load 5ms on PLT*/
+
+    LDST(p->p_s) /*Load Processor State*/
+
+
 }
