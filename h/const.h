83ec7a2815838b0c54bf6062cf97f9b26b76ba5b
countsj1 <countsj1@xavier.edu>
2022-10-03 22:43:55 -0400

    Updated initial.c and const.h

--- h/const.h
+++ h/const.h
@@ -8,35 +8,43 @@
  ****************************************************************************/
 
 /* Hardware & software constants */
-#define PAGESIZE		  4096			/* page size in bytes	*/
-#define WORDLEN			  4				  /* word size in bytes	*/
+#define PAGESIZE		  4096			/* page size in bytes */
+#define WORDLEN			  4				/* word size in bytes */
 
 /* define MAXPROC */
-#define MAXPROC         20
+#define MAXPROC           20
 
 /* define MAXINT */
-#define MAXINT           ((void *)0xFFFFFFFF)
+#define MAXINT            ((void *)0xFFFFFFFF)
 
 /* define MAXDEVICECNT */
-#define MAXDEVICECNT    49
+#define MAXDEVICECNT      49
 
 /* define top of the Nucleus stack page */
-#define TOP     0x20001000
+#define KERNALSTACK       0x20001000
+
+/* define */
+#define ALLOFF            0x00000000
+#define USERPON           0x00000008
+#define IEPON             0x00000004
+#define IEMON             0x00000001
+#define IMON              0x0000FF00
+#define TEBITON           0x00000000
 
 /* timer, timescale, TOD-LO and other bus regs */
-#define RAMBASEADDR		0x10000000
-#define RAMBASESIZE		0x10000004
+#define RAMBASEADDR		  0x10000000
+#define RAMBASESIZE		  0x10000004
 #define TODLOADDR		  0x1000001C
-#define INTERVALTMR		0x10000020	
-#define TIMESCALEADDR	0x10000024
+#define INTERVALTMR		  0x10000020	
+#define TIMESCALEADDR	  0x10000024
 
 /* utility constants */
-#define	TRUE			    1
-#define	FALSE			    0
+#define	TRUE			  1
+#define	FALSE			  0
 #define HIDDEN			  static
-#define EOS				    '\0'
+#define EOS				  '\0'
 
-#define NULL 			    ((void *)0xFFFFFFFF)
+#define NULL 			  ((void *)0xFFFFFFFF)
 
 /* device interrupts */
 #define DISKINT			  3
@@ -48,42 +56,41 @@
 #define DEVINTNUM		  5		  /* interrupt lines used by devices */
 #define DEVPERINT		  8		  /* devices per interrupt line */
 #define DEVREGLEN		  4		  /* device register field length in bytes, and regs per dev */	
-#define DEVREGSIZE	  16 		/* device register size in bytes */
+#define DEVREGSIZE	      16 		/* device register size in bytes */
 
 /* device register field number for non-terminal devices */
 #define STATUS			  0
 #define COMMAND			  1
-#define DATA0			    2
-#define DATA1			    3
+#define DATA0			  2
+#define DATA1			  3
 
 /* device register field number for terminal devices */
-#define RECVSTATUS  	0
-#define RECVCOMMAND 	1
-#define TRANSTATUS  	2
-#define TRANCOMMAND 	3
+#define RECVSTATUS  	  0
+#define RECVCOMMAND 	  1
+#define TRANSTATUS  	  2
+#define TRANCOMMAND 	  3
 
 /* device common STATUS codes */
-#define UNINSTALLED		0
-#define READY			    1
-#define BUSY			    3
+#define UNINSTALLED		  0
+#define READY			  1
+#define BUSY			  3
 
 /* device common COMMAND codes */
-#define RESET			    0
-#define ACK				    1
+#define RESET			  0
+#define ACK				  1
 
 /* Memory related constants */
-#define KSEG0           0x00000000
-#define KSEG1           0x20000000
-#define KSEG2           0x40000000
-#define KUSEG           0x80000000
-#define RAMSTART        0x20000000
-#define BIOSDATAPAGE    0x0FFFF000
+#define KSEG0             0x00000000
+#define KSEG1             0x20000000
+#define KSEG2             0x40000000
+#define KUSEG             0x80000000
+#define RAMSTART          0x20000000
+#define BIOSDATAPAGE      0x0FFFF000
 #define	PASSUPVECTOR	  0x0FFFF900
 
 /* Exceptions related constants */
 #define	PGFAULTEXCEPT	  0
 #define GENERALEXCEPT	  1
-
 
 /* operations */
 #define	MIN(A,B)		((A) < (B) ? A : B)
--- phase2/initial.c
+++ phase2/initial.c
@@ -1,24 +1,51 @@
 #include "../h/pcb.h"
 #include "../h/types.h"
 #include "../h/const.h"
+#include "../h/exceptions.h"
+#include "../h/scheduler.h"
+#include "../h/interrupts.h"
 #include "p2test.c"
+
+/************************************ Nucleus Initialization ****************************
+ *
+ *   cd on  
+ * 
+ *   Authors:
+ *      Ronnie Cole
+ *      Joe Pinkerton
+ *      Joseph Counts
+*/
+
+extern void test();
+extern void uTLB_RefillHandler();
 
 /* 
     Declare global variables 
 */
-int procssCnt;      /* int indicating the number of strated, but not yet terminated processes */
-int softBlockCnt;   /* number of started, but not terminated processes that are in the "blocked" statevdue to an I/O or timer request*/
-pcb_t *readyQue;   /* tail pointer to a queue of pcbs that are in the "ready" state */
-pcb_t *currentProc;  /* pointer to the pcb that is in the "running" state */
+int procssCnt;          /* int indicating the number of strated, but not yet terminated processes */
+int softBlockCnt;       /* number of started, but not terminated processes that are in the "blocked" statevdue to an I/O or timer request*/
+pcb_t *readyQue;        /* tail pointer to a queue of pcbs that are in the "ready" state */
+pcb_t *currentProc;     /* pointer to the pcb that is in the "running" state */
 int deviceSema4s[MAXDEVICECNT]; 
 
+/* 
+    Programs entry point performing the Nucleus initialization
+*/
 int main(){
+    /* Load system-wide interval timer */
+    int RAMTOP;
+    devregarea_t *top;
+    top = (devregarea_t *) RAMBASEADDR;
+    RAMTOP = top->rambase + top->ramsize;
+    top->intervaltimer = 100;               /* 100 milliseconds */
+
     /* Populate the Processor 0 Pass Up Vector */
-    passupvector_t temp;
-    temp->tlb_refill_handler = (memaddr) uTLB_RefillHandler;
-    temp->tlb_refill_stackPtr = TOP;
-    temp->exception_handler = (memaddr) fooBar;
-    temp->exception_stackPtr = TOP;
+    passupvector_t *temp;
+    temp = (passupvector_t *) ;
+    temp->tlb_refill_handler = (memaddr) uTLB_RefillHandler();
+    temp->tlb_refill_stackPtr = KERNELSTACK;
+    temp->exception_handler = (memaddr) genExceptionHandler();
+    temp->exception_stackPtr = KERNELSTACK;
 
     /* Initialize PCB and ASL data structures */
     initPcbs();
@@ -34,20 +61,26 @@ int main(){
         deviceSema4s[i] = 0;
     }
 
-    /* Load system-wide Interval Timer with 100 milliseconds */
-    INTERVALTMR
-
     /* Instantiate a single process */
     currentProc = allocPcb();
-    procssCnt++;
+    if(currentProc != NULL){
+        currentProc->p_s.s_sp = RAMTOP
+        currentProc->p_s.s_pc = (memaddr) test; /* test function in p2test */
+        currentProc->p_s.s_t9 = (memaddr) test;
+        currentProc->p_s.s_status = ALLOFF | IEPON | IMON | TEBITON;
+        insertProcQ(&readyQue, currentProc);
+        procssCnt += 1;
+        currentProc = NULL;
+    } else{
+        PANIC();
+    }
 
-    currentProc->p_s.s_pc = (memaddr) test; /* test function in p2test */
-
-    insertProcQ(&readyQue, currentProc);
-    currentProc = NULL;
-
-    extern void test();
+    LDIT();
 
     /* Call the Scheduler */
     scheduler();
+}
+
+void genExceptionHandler(){
+
 }
