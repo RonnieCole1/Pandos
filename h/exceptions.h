df2f70c5e1083128ac951441a7b7afc534599fa4
countsj1 <countsj1@xavier.edu>
2022-11-04 13:14:37 -0400

    Updated exceptions.h

--- h/exceptions.h
+++ h/exceptions.h
@@ -14,13 +14,14 @@
 #include "../h/types.h"
 
 extern void SYSCALL();
-extern void Create_ProcessP();
+extern void Create_ProcessP(state_t* caller);
 extern void Terminate_Process();
-extern pcb_t *wait(sema4);
-extern pcb_t *signal(sema4);
+extern pcb_t *wait(int sema4);
+extern pcb_t *signal(int sema4);
 extern void Wait_for_IO_Device();
 extern int Get_CPU_Time(pcb_t *p);
 extern void Wait_For_Clock();
 extern void BlockedSYS(pcb_t *p);
+extern void sys2Helper(pcb_PTR head);
 
 #endif
